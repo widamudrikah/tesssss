30f303889ee11aff50dc5df1ffa340720543a366
widamudrikah <mudrikahwida@gmail.com>
2023-08-25 14:17:45 +0700

    first commit

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+let menuIcon = document.querySelector('#menu-icon')
+let navbar = document.querySelector('.navbar')
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+}
+
+window.onscroll = () => {
+    menuIcon.classList.remove('bx-x')
+    navbar.classList.remove('active')
+}
